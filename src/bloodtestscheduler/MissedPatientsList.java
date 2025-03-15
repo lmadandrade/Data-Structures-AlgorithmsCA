729bf846b0f0bff2e4d940e691b7e9d0db3edbc0
lmadandrade <lucas.matheus.alves14@live.com>
2025-03-15 17:30:20 +0000

    LinkedList implementation on MissedPatientsList

--- src/bloodtestscheduler/MissedPatientsList.java
+++ src/bloodtestscheduler/MissedPatientsList.java
@@ -4,10 +4,41 @@
  */
 package bloodtestscheduler;
 
+import java.util.LinkedList;
+import java.util.List;
+
 /**
  *
  * @author lucasandrade
+ * 
+ * Tracks the last 5 patients who did not show up for their appointment 
+ * Uses a List to store 5 no-shows patients
  */
+
 public class MissedPatientsList {
+    private List<Patient> missedPatients; // stores the last 5 no-show patients
+    
+    // constructor:
+    
+    public MissedPatientsList(){
+        this.missedPatients = new LinkedList<>();
+    }
+    
+    public void addMissedPatient(Patient patient) {
+        if (missedPatients.size() == 5) {
+            missedPatients.remove(0); // this will remove the oldest no-show patient
+        }
+        missedPatients.add(patient);
+    }
+    
+    // Retrieving the list of the last 5 no-show patients:
+    public List<Patient> getMissedPatients() {
+        return missedPatients;
+    }
+    
+    // Checking if there are no missed patients
+    public boolean isEmpty() {
+        return missedPatients.isEmpty();
+    }
     
 }
